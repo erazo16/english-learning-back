c61380e08ba7dc2d206373ebd3221ed483db500b
erazo <erazorodriguezjuanesteban@gmail.com>
2026-03-10 18:51:13 -0500

    second commit

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://english-learning-back-production.up.railway.app',
+      'https://aprendiendo-con-amor.netlify.app',
+    ],
     credentials: true,
   });
 
